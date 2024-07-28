ed2cf3c7926458380da03aef97edd1f453ac792e
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-07-28 20:06:12 +0530

    update

--- QR-code/QR-Code.js
+++ QR-code/QR-Code.js
@@ -1,3 +1,19 @@
+const qrinput = document.getElementById("qr-input");
+const qrimg = document.getElementById("qr-img");
+const qrbtn = document.getElementById("qr-btn");
+// console.log(qrbtn,qrimg,qrinput);
+qrbtn.addEventListener('click',()=>{
+    const inputValue = qrinput.value;
+    // console.log(inputValue);
+    if(inputValue === ''){
+        alert('please enter a valid URL');
+        return
+    }else{
+        qrimg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`
+        qrimg.alt=`QR code for ${inputValue}`
+    }
+});
+
 
 
   
